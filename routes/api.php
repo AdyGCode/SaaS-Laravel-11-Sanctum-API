620d86d2f520ac401ccc2f8eaf22fcc09c32164d
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2024-08-15 16:35:01 +0800

    feat(product): Initial product feature and tests

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('/products', ProductController::class);
+});
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
